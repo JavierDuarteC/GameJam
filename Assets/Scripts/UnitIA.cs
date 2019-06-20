3d45ec97cbc11a64a27f2710274590d5e1ce9d97
Jesús Montoya <gsus.montoya.17@gmail.com>
2019-06-20 12:08:03 -0500

    Ataque en linea con bugs

--- Assets/Scripts/UnitIA.cs
+++ Assets/Scripts/UnitIA.cs
@@ -1,6 +1,4 @@
-﻿using System;
-using System.Collections;
-using System.Collections.Generic;
+﻿using System.Collections;
 using UnityEngine;
 
 public class UnitIA : MonoBehaviour
@@ -20,16 +18,17 @@ public class UnitIA : MonoBehaviour
 
     // Cantidad de vida que resta
     public int attackPower;
+    private GameObject objAttack;
 
     // Velocidad de Ataque
-    public float attackRate = 1f;
-    private float _attackCountdown = 0;
-
+    [SerializeField] private float attackSpeed;
+    private float randomSpeed;
 
     // Start is called before the first frame update
     void Awake()
     {
         target = GameObject.FindGameObjectWithTag(isEnemy ? "Torre" : "TorreEnemiga");
+        randomSpeed = Random.Range(attackSpeed, attackSpeed + 0.1f);
     }
 
     // Update is called once per frame
@@ -40,43 +39,50 @@ public class UnitIA : MonoBehaviour
             new Vector3(target.transform.position.x, transform.position.y, 0f), step);
     }
 
-
     private void OnTriggerEnter2D(Collider2D col2D)
     {
         if (isEnemy)
         {
             if (col2D.CompareTag("UnitIA") || col2D.CompareTag("Torre"))
-                Attack(col2D);
+                AttackOver(col2D);
         }
-
         else
         {
             if (col2D.CompareTag("EnemyIA") || col2D.CompareTag("TorreEnemiga"))
-                Attack(col2D);
+                AttackOver(col2D);
         }
     }
 
-    private void Attack(Collider2D other)
+    private void Attack(UnitIA other)
     {
-        // TODO: Corregir el bug del Emprendedor que mata todo
-        var otherStats = other.gameObject.GetComponent<UnitIA>();
-        while (otherStats.lifePoints > 0 && lifePoints > 0)
+        
+        other.lifePoints -= attackPower;
+        print("[" + gameObject.name + "] attack to [" + other.gameObject.name + "] with [" + randomSpeed +
+              "] Aspd");
+        print("Vida de " + gameObject.name + " --> " + lifePoints);
+        print("Vida de: " + other.gameObject.name + "-->" + other.lifePoints);
+
+        if (other.lifePoints <= 0)
         {
-            print("[" + gameObject.name + "] attack to [" + other.name + "]");
-            if (_attackCountdown <= 0)
-            {
-                otherStats.lifePoints -= attackPower;
-                if (otherStats.lifePoints <= 0)
-                {
-                    Destroy(other.gameObject);
-                }
-
-                _attackCountdown = 1f / attackRate;
-            }
-
-            print("Vida resultante del objetivo:" + otherStats.lifePoints);
-
-            _attackCountdown -= Time.deltaTime;
+            print("Muere > " + other.gameObject.name);
+            Destroy(other.gameObject);
         }
     }
+
+    private void AttackOver(Collider2D col2D)
+    {
+        objAttack = col2D.gameObject;
+        var otherStats = objAttack.GetComponent<UnitIA>();
+
+        if (lifePoints > 0 && otherStats.lifePoints > 0)
+        {
+            //TODO esperar el atk speed con un contador de ticks while
+            Attack(otherStats);
+        }
+        else
+        {
+            objAttack = null;
+        }
+    }
+    
 }
