3c87f055aa22ee30308424358ec934fd26959324
Jesús Montoya <gsus.montoya.17@gmail.com>
2019-06-24 17:03:13 -0500

    Menu principal sin fondo

---
+++ Assets/Scripts/Navigator.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Navigator : MonoBehaviour
+{
+    [Header("Nombre de la escena siguiente:")]
+    public String sceneName;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            SceneManager.LoadScene(sceneName: sceneName);
+        }
+    }
+
+    public void NextScene()
+    {
+        SceneManager.LoadScene(sceneName: sceneName);
+    }
+
+    public void Close()
+    {
+        Application.Quit();
+    }
+}
